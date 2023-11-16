8d8b4865ed85b71c5c4db5336f914f22fac37ba6
elmaouadda <43704088+elmaouadda@users.noreply.github.com>
2023-11-16 01:14:09 +0100

    Add files via upload
    Project Title: Shopping Cart Management
    
    Description:
    This project implements a simple shopping cart management system using HTML, CSS, and JavaScript. Users can view a list of products, add items to the cart, and adjust the quantities of items in the cart. The shopping cart is stored in the browser's local storage for persistence between sessions.
    
    Key Features:
    - View a list of products with images, names, descriptions, and prices.
    - Add items to the shopping cart.
    - Increment and decrement quantities of items in the cart.
    - Dynamically update the displayed quantity on the page.
    - Utilizes local storage for cart persistence.
    
    Usage:
    1. Open the `index.html` file in a web browser.
    2. Browse the available products and use the "Add to Cart" button to add items.
    3. Adjust item quantities in the cart using the increment and decrement buttons.
    4. View the updated total quantity in the cart icon.
    
    Note:
    This project is a simple demonstration of shopping cart functionality and can be used as a foundation for more complex e-commerce applications.

---
+++ src/cart.js
@@ -0,0 +1,147 @@
+let label = document.getElementById('label');
+let shoppingCart = document.getElementById('shopping-cart');
+
+
+
+let basket = JSON.parse(localStorage.getItem('basket')) || [];
+
+//console.log(basket);
+
+let calculation = () => {
+  
+  let cartIcon = document.getElementById('cartAmount');
+  cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x, y)=> x + y, 0);
+
+ // console.log(basket.map((x)=>x.item).reduce((x, y)=> x + y, 0));
+}
+
+calculation();
+
+let generateCartItems = () => {
+  if (basket.length !==0){
+    return (shoppingCart.innerHTML = basket.map((x)=>{
+     // console.log(x);
+      let {id, item} = x;
+      let search = shopItemsData.find((y)=>y.id === id) || [];
+      return `
+            <div class="cart-item">
+                  <img width="100" src=${search.img} alt="" />
+
+                  <div class="details">
+                  
+                      <div class="title-price-x">
+                            <h4 class="title-price">
+                              <p>${search.name} </p>
+                              <p class="cart-item-price">$ ${search.price} </p>
+
+                            </h4>
+                            <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+                      </div>
+
+           <div class="buttons">
+                      <i onclick="decrement(${id})" class="bi bi-dash"></i>
+                      <div id=${id} class="quantity js-quantity">${item}</div>
+                      <i onclick="increment(${id})" class="bi bi-plus"></i>
+          </div>
+
+                      <h3>$ ${item * search.price}</h3>
+                  
+                  </div>
+            
+            </div>
+      
+      `
+    } ).join(''));
+  }
+  else{
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty <h2>
+    <a href="index.html">
+          <button class="home-button">Back to home</button>
+    </a>
+    `;
+  }
+};
+
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  generateCartItems();
+  update(selectedItem.id);
+
+  localStorage.setItem('basket', JSON.stringify(basket));
+}
+
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search && search.item > 0) {
+    search.item -= 1;
+    update(selectedItem.id);
+  }
+
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  localStorage.setItem('basket', JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x)=> x.id === id)
+ // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  totalAmount();
+
+};
+
+
+let removeItem =(id)=>{
+  let selectedItem = id;
+ // console.log(selectedItem);
+ basket = basket.filter((x)=> x.id !== selectedItem.id);
+ generateCartItems();
+ totalAmount();
+ calculation();
+ localStorage.setItem('basket', JSON.stringify(basket));
+ 
+};
+
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  calculation();
+  localStorage.setItem('basket', JSON.stringify(basket));
+}
+
+let totalAmount = ()=> {
+  if(basket.length !==0){
+    let amount = basket.map((x)=> {
+        let {item, id} = x;
+        let search = shopItemsData.find((y)=>y.id === id) || [];
+        return item * search.price;
+    }).reduce((x,y)=>x+y, 0)
+   // console.log(amount);
+   label.innerHTML = `
+   <h2> Total Bill : $ ${amount}</h2>
+   <button class="checkout">Checkout</button>
+  <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+
+   `;
+  } else return;
+}
+
+totalAmount();
